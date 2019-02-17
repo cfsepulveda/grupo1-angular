df11c7d19cd1b4dc436665c16ea50adcae9bb20a
cfsepulveda <42418118+cfsepulveda@users.noreply.github.com>
2019-02-17 13:19:18 -0500

    cors

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.options('*', cors());
 app.use(express.static(__dirname + '/dist/angular'));
 app.use(cors())
 
+app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
 
 app.get('/*', function(req,res) {
     
