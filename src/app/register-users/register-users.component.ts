a72ae62cb3b93ea5f3a006af82fb571fc722b2bb
Alejandro Garcia Flores <a.garcia13@uniandes.edu.co>
2019-02-24 16:05:29 -0500

    formulario con imagen

--- src/app/register-users/register-users.component.ts
+++ src/app/register-users/register-users.component.ts
@@ -21,9 +21,9 @@ export class RegisterUsersComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = new FormGroup({
-      username: new FormControl(),
-      first_name: new FormControl(),
-      last_name: new FormControl(),
+      login: new FormControl(),
+      name: new FormControl(),
+      lastname: new FormControl(),
       password: new FormControl(),
       email: new FormControl()
     });
@@ -31,12 +31,16 @@ export class RegisterUsersComponent implements OnInit {
   }
 
   register() {
-    const username = this.registerForm.get('username').value; 
+    const login = this.registerForm.get('login').value; 
     const password = this.registerForm.get('password').value;
-    const firstName = this.registerForm.get('first_name').value; 
-    const lastName = this.registerForm.get('last_name').value;
+    const name = this.registerForm.get('name').value; 
+    const lastname = this.registerForm.get('lastname').value;
     const email = this.registerForm.get('email').value;
-    const user = {'username': username, 'password': password,'first_name': firstName, 'last_name': lastName, 'email': email};
+    const city = this.registerForm.get('city').value;
+    const country = this.registerForm.get('country').value;
+    const photo = this.registerForm.get('photo').value;
+    const photoFile = this.registerForm.get('photoFile').value;
+    const user = {'login': login, 'password': password,'name': name, 'lastname': lastname, 'email': email, 'photo':photo, 'city':city, 'country':country};
     const resultado = this.registerUsersRestClientService.register(user);
     console.log(resultado);
     this.goBack();
